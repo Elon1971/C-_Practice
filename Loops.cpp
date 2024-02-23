4292e212c1177fd81881fea0d1035647103fc2c4
Elon1971 <94976777+Elon1971@users.noreply.github.com>
2024-02-23 16:54:25 +0530

    Add files via upload

--- Loops.cpp
+++ Loops.cpp
@@ -109,3 +109,99 @@ int main() {
 
     return 0;
 }
+
+
+
+// Exercise 6
+
+// Write a program that prints out in the console a simple representation of 3x3 plane. The plane
+// should be filled with numbers from 1 to 9 (stored in 3x3 array representing x,y coordinates of
+// the plane).
+
+
+
+//     1     2     3
+
+
+
+//     4     5     6
+
+
+
+//     7     8     9
+
+
+// The value from coordinate (1, 1) is 1 (array[0][0]).
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main() {
+
+    int arr[3][3] = {};
+    int a=1, b=1;
+    int x, y;
+
+    cout << "Enter the index you want to put the 'X'" << endl;
+
+
+
+
+    while (b <= 9) {
+        if (b%2 != 0) {
+            cout << "First Player's turn: " << endl;
+            cout << "Enter x value: ";
+            cin >> x;
+            cout << "Enter y value: ";
+            cin >> y ;
+
+            x--;
+            y--;
+
+            for (int i=0; i<3; i++) {
+                for (int j=0; j<3; j++) {
+                    if (x == i && y == j) {
+                        cout << 'X' << " ";
+
+                    }
+                    else {
+                        cout << a++ << " ";
+                    }
+                }
+                cout << endl;
+            }
+            arr[x][y] = 'X';
+        }
+        else {
+            cout << "Second Player's turn: " << endl;
+            cout << "Enter x value: ";
+            cin >> x;
+            cout << "Enter y value: ";
+            cin >> y ;
+
+            x--;
+            y--;
+
+            for (int i=0; i<3; i++) {
+                for (int j=0; j<3; j++) {
+                    if (x == i && y == j) {
+                        cout << 'O' << " ";
+
+                    }
+                    else {
+                        cout << a++ << " ";
+                    }
+                }
+                cout << endl;
+            }
+            arr[x][y] = 'O';
+        }
+        a=0;
+        b++;
+
+    }
+
+
+    return 0;
+}
