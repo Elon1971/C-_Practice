8a7ac4765d31b6e3f91684fab4ebca3d1f62be92
Elon1971 <94976777+Elon1971@users.noreply.github.com>
2024-02-23 11:23:02 +0530

    Add files via upload

--- Switch_Case.cpp
+++ Switch_Case.cpp
@@ -1,3 +1,40 @@
+//Exercise 1
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int choice;
+    cout << "Enter the Choice: ";
+    cin >> choice;
+
+    switch(choice) {
+
+        case 1:
+            cout << "Your Coke is given to you";
+            break;
+        case 2:
+            cout << "Your Water is given to you";
+            break;
+        case 3:
+            cout << "Your Bar is given to you";
+            break;
+        case 4:
+            cout << "Your Choclate is given to you";
+            break;
+        case 5:
+            cout << "Your Fanta is given to you";
+            break;
+        default:
+            cout << "Wrong Input";
+            break;
+    }
+
+    return 0;
+}
+
+//Exercise 2
+
 #include <iostream>
 using namespace std;
 
